021239451c6fd2e31b97ebe77c999efad1e0b857
Lucas de Alcantara Noblat <lucasan@dcc.ufrj.br>
2025-03-27 09:57:55 -0300

    Transferi a view de plotagem do bokeh e inseri as abinhas de settings & unitys

--- Django/osciloscopio/models.py
+++ Django/osciloscopio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class dataSource(models.Model):
+    x_value = models.FloatField()
+    y_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- Django/osciloscopio/views.py
+++ Django/osciloscopio/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Bokeh
+
+from bokeh.plotting import figure, show # Para criar a figure e mostra-la
+from bokeh.io import output_notebook  # Para exibir no Jupyter Notebook
+from bokeh.models import ColumnDataSource # Para atualizar em tempo real
+from bokeh.palettes import Category10  # Paleta de cores para os sinais
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+
+import warnings
+
 # Create your views here.
 
 def unidades(request):
@@ -12,3 +24,21 @@ def unidades(request):
     
 def configuracoes(request):
     return render(request, 'configuracoes/configuracoes.html')
+
+'''
+
+def bokeh_view(request):
+    # Crie um gráfico simples
+    plot = figure(title="Gráfico Bokeh", x_axis_label='X', y_axis_label='Y')
+    plot.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=2)
+    
+    # Gere os componentes JavaScript e HTML
+    script, div = components(plot)
+    
+    return render(request, 'bokeh_template.html', {
+        'bokeh_script': script,
+        'bokeh_div': div
+    })
+
+
+'''
