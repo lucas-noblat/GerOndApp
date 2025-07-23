110a852d0e63a87f3807fa632c2b0ff2fdd4a352
lucas-noblat <lucasan@dcc.ufrj.br>
2025-07-23 09:46:24 -0300

    Adicionei dockerfile

--- Django/GerOndApp/settings.py
+++ Django/GerOndApp/settings.py
@@ -21,11 +21,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if ENVIRONMENT == 'development' else False
 
-ALLOWED_HOSTS = [
-    h.strip() for h in (env('ALLOWED_HOSTS', default='').split(','))
-    if h.strip()
-]
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 # Application definition
 
