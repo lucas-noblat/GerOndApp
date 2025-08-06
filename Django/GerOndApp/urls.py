e587ec770661d4ded469980e45a3316c77e69bcd
Lucas Noblat <lucasan@dcc.ufrj.br>
2025-08-06 18:14:04 -0300

    Commit final antes do primeiro Release

--- Django/GerOndApp/urls.py
+++ Django/GerOndApp/urls.py
@@ -17,7 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+####### Para desenvolvimento ###############
+from django.conf import settings
+from django.conf.urls.static import static
+
+############################################
+
 urlpatterns = [
     path('', include('home.urls')),
     path('api/', include('api.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
